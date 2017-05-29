04b9c8e0d62f351cb11f607b902856180ebfe1b3
Kasper Tvede <kasper@webmasteren.eu>
2017-05-29 20:19:25 +0200

    added some extensions for typed arrays.

---
+++ com.commonsense.android.kotlin/src/main/java/com/commonsense/android/kotlin/android/extensions/TypedArrayExtensions.kt
@@ -0,0 +1,39 @@
+package com.commonsense.android.kotlin.android.extensions
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.graphics.drawable.Drawable
+import android.support.annotation.DrawableRes
+import android.support.annotation.StyleRes
+import android.support.v7.content.res.AppCompatResources
+
+/**
+ * Created by kasper on 29/05/2017.
+ */
+
+
+fun TypedArray.getDrawableSafe(@StyleRes style: Int, context: Context): Drawable? {
+    return ifHaveOrNull(style) {
+        val defValue = -1
+        @DrawableRes
+        val drawableRes = getResourceId(style, defValue)
+        if (drawableRes != defValue) {
+            return@ifHaveOrNull AppCompatResources.getDrawable(context, drawableRes)
+        }
+        return@ifHaveOrNull null
+    }
+}
+
+fun TypedArray.getTextSafe(@StyleRes style: Int): CharSequence? {
+    return ifHaveOrNull(style) {
+        return@ifHaveOrNull getText(style)
+    }
+}
+
+
+inline fun <T> TypedArray.ifHaveOrNull(@StyleRes style: Int, crossinline actionIf: () -> T?): T? {
+    if (hasValue(style)) {
+        return actionIf()
+    }
+    return null
+}

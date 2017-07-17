597e2e0654f163edcaf0f5a466e0a751782e99d5
Kasper Tvede <kasper@webmasteren.eu>
2017-07-17 07:52:19 +0200

    almost working soft/hard keyboard input handling.

--- com.commonsense.android.kotlin/src/main/java/com/commonsense/android/kotlin/widgets/ExtendedEditTextView.kt
+++ com.commonsense.android.kotlin/src/main/java/com/commonsense/android/kotlin/widgets/ExtendedEditTextView.kt
@@ -1,8 +1,11 @@
 package com.commonsense.android.kotlin.widgets
 
-import android.text.InputFilter
 import android.support.v7.widget.AppCompatEditText
 import android.text.TextWatcher
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputConnection
+import android.view.inputmethod.InputConnectionWrapper
 
 /**
  * Created by kasper on 10/07/2017.
@@ -21,17 +24,8 @@ class ExtendedEditTextView : AppCompatEditText {
     }
 
 
-    private val ourFilter =
-            InputFilter { charSequence, start, end, dest, dStart, dEnd ->
-                if (end == 0) {
-                    backspaceDetectedCallback?.invoke(this, text.isEmpty())
-                }
-                return@InputFilter charSequence
-            }
-
-
     private fun afterInit() {
-        filters = null
+
     }
 
     private val internalListOfListeners = mutableListOf<TextWatcher>()
@@ -54,26 +48,56 @@ class ExtendedEditTextView : AppCompatEditText {
         internalListOfListeners.remove(watcher)
     }
 
-    override fun setFilters(filters: Array<out InputFilter>?) {
-        val array: Array<out InputFilter> = if (filters != null) {
-            arrayOf(getOurInputFilter(), *filters)
-        } else {
-            arrayOf(getOurInputFilter())
-        }
-        super.setFilters(array)
+
+    override fun onCreateInputConnection(outAttrs: EditorInfo?): InputConnection {
+        return KeyboardConnection(super.onCreateInputConnection(outAttrs), true, this::onKeyboardEvent)
     }
 
-
-    private fun getOurInputFilter(): InputFilter {
-        return InputFilter { charSequence, start, end, dest, dStart, dEnd ->
-            if (end == 0) {
-                backspaceDetectedCallback?.invoke(this, text.isEmpty())
-            }
-            return@InputFilter charSequence
+    //TODO listen for custom sequences ? (hmm ) would avoid opening this func.
+    private fun onKeyboardEvent(event: KeyEvent): Boolean {
+        if (event.action == KeyEvent.ACTION_DOWN && event.keyCode == KeyEvent.KEYCODE_DEL) {
+            backspaceDetectedCallback?.invoke(this, length() == 0)
         }
+        return false
     }
 
     fun setOnBackSpaceDeteced(callback: (view: ExtendedEditTextView, isEmpty: Boolean) -> Unit) {
         backspaceDetectedCallback = callback
+    }
+
+    //hardware keyboard
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (event != null && onKeyboardEvent(event)) {
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+}
+
+typealias onKeyboardEvent = (KeyEvent) -> Boolean
+
+private class KeyboardConnection(target: InputConnection,
+                                 mutable: Boolean,
+                                 val callback: onKeyboardEvent)
+    : InputConnectionWrapper(target, mutable) {
+
+    override fun sendKeyEvent(event: KeyEvent?): Boolean {
+        if (event != null && callback(event)) {
+            return true
+        }
+        return super.sendKeyEvent(event)
+
+    }
+
+    override fun deleteSurroundingText(beforeLength: Int, afterLength: Int): Boolean {
+        // magic: in latest Android, deleteSurroundingText(1, 0) will be called for backspace, when there are no text..
+        return if (beforeLength == 1 && afterLength == 0) {
+            // emulate backspace event..
+            sendKeyEvent(KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DEL))
+                    && sendKeyEvent(KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_DEL))
+        } else {
+            super.deleteSurroundingText(beforeLength, afterLength)
+        }
+
     }
 }

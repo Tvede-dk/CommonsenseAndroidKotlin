f2e808efdcc93d350be0dc63b78cf980bbd2426f
Kasper Tvede <kasper@webmasteren.eu>
2017-07-27 15:28:11 +0200

    hook into the snack view. allows for customizations. and a edtion without action as well.

--- com.commonsense.android.kotlin-views/src/main/java/com/commonsense/android/kotlin/views/features/SnackbarHelper.kt
+++ com.commonsense.android.kotlin-views/src/main/java/com/commonsense/android/kotlin/views/features/SnackbarHelper.kt
@@ -6,6 +6,7 @@ import android.support.design.widget.BaseTransientBottomBar
 import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
 import android.view.View
+import com.commonsense.android.kotlin.base.FunctionUnit
 
 /**
  * Created by kasper on 12/07/2017.
@@ -19,6 +20,7 @@ fun Fragment.showSnackbar(view: View,
                           @IntRange(from = 0)
                           durationInMs: Int,
                           onAction: () -> Unit,
+                          modifyView: FunctionUnit<View>? = null,
                           onTimeout: EmptyMethodNullable = null,
                           onDismissOtherwise: EmptyMethodNullable = null): Snackbar {
 
@@ -37,6 +39,33 @@ fun Fragment.showSnackbar(view: View,
             }
         }
     })
+    modifyView?.invoke(mySnackbar.view)
+    mySnackbar.show()
+    return mySnackbar
+}
+
+
+fun Fragment.showSnackbarNoAction(view: View,
+                                  @StringRes text: Int,
+                                  @IntRange(from = 0)
+                                  durationInMs: Int,
+                                  modifyView: FunctionUnit<View>? = null,
+                                  onTimeout: EmptyMethodNullable = null,
+                                  onDismissOtherwise: EmptyMethodNullable = null): Snackbar {
+
+    val mySnackbar = Snackbar.make(view,
+            text, Snackbar.LENGTH_SHORT)
+    mySnackbar.duration = durationInMs
+    mySnackbar.addCallback(object : BaseTransientBottomBar.BaseCallback<Snackbar?>() {
+        override fun onDismissed(transientBottomBar: Snackbar?, event: Int) {
+            super.onDismissed(transientBottomBar, event)
+            when {
+                event == DISMISS_EVENT_TIMEOUT -> onTimeout?.invoke()
+                event != DISMISS_EVENT_ACTION -> onDismissOtherwise?.invoke()
+            }
+        }
+    })
+    modifyView?.invoke(mySnackbar.view)
     mySnackbar.show()
     return mySnackbar
 }

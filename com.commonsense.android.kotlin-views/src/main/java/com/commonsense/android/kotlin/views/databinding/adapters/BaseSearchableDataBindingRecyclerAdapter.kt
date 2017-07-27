c819f41ce48fac2857f101e462809d61c430ffc0
Kasper Tvede <kasper@webmasteren.eu>
2017-07-28 01:33:45 +0200

    comments, and wrapped almost all calls into filter functions. should preserve filter accross operations. still missing 1 impl.

--- com.commonsense.android.kotlin-views/src/main/java/com/commonsense/android/kotlin/views/databinding/adapters/BaseSearchableDataBindingRecyclerAdapter.kt
+++ com.commonsense.android.kotlin-views/src/main/java/com/commonsense/android/kotlin/views/databinding/adapters/BaseSearchableDataBindingRecyclerAdapter.kt
@@ -3,6 +3,7 @@ package com.commonsense.android.kotlin.views.databinding.adapters
 import android.content.Context
 import android.databinding.ViewDataBinding
 import android.support.v7.widget.RecyclerView
+import com.commonsense.android.kotlin.base.FunctionUnit
 import com.commonsense.android.kotlin.system.datastructures.TypeSection
 import com.commonsense.android.kotlin.system.datastructures.TypeSectionLookupRepresentative
 import com.commonsense.android.kotlin.system.logging.L
@@ -51,6 +52,7 @@ open class AbstractSearchableDataBindingRecyclerAdapter<
         F>(context: Context)
     : DataBindingRecyclerAdapter<T>(context.applicationContext) {
 
+    //<editor-fold desc="Field variables">
     private val allDataCollection: TypeSectionLookupRepresentative<T, InflatingFunction<*>>
             = TypeSectionLookupRepresentative()
 
@@ -58,8 +60,9 @@ open class AbstractSearchableDataBindingRecyclerAdapter<
 
     //the "gatekeeper" for our filter function. will restrict acces so only one gets in. thus if we spam the filter, we should only use the latest filter.
     private val filterActor: ConflatedActorHelper<F> = ConflatedActorHelper()
+    //</editor-fold>
 
-
+    //<editor-fold desc="Filter suspend (background)">
     private suspend fun filterBySuspend(filter: F?) {
         try {
             if (filter != filterValue) {
@@ -67,7 +70,7 @@ open class AbstractSearchableDataBindingRecyclerAdapter<
             }
             L.error("filter", "on " + allDataCollection.size)
             val items = allDataCollection.mapAll {
-                it.filter { isAcceptedByFilter(it, filter) }
+                it.filterByOurFilter()
             }
 
             L.error("filter", "resulted in " + items.size)
@@ -77,67 +80,105 @@ open class AbstractSearchableDataBindingRecyclerAdapter<
         }
     }
 
+    private suspend fun updateVisibly(data: List<TypeSection<T>>) {
+        launch(UI) {
+            super.setAllSections(data)
+        }.join()
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="add">
     override fun add(newItem: T, inSection: Int) {
         allDataCollection.add(newItem, inSection)
-        performActionIfIsValidFilter(newItem, { super.add(newItem, inSection) })
+        performActionIfIsValidFilter(newItem) {
+            super.add(newItem, inSection)
+        }
     }
 
     override fun addAll(items: Collection<T>, inSection: Int) {
         allDataCollection.addAll(items, inSection)
-        items.forEach {
-            performActionIfIsValidFilter(it, { super.add(it, inSection) })
-        }
+        super.addAll(items.filterByOurFilter(), inSection)
     }
 
-    private inline fun performActionIfIsValidFilter(newItem: T, crossinline action: (T) -> Unit) {
-        if (isAcceptedByFilter(newItem, filterValue)) {
-            action(newItem)
-        }
-    }
 
+    override fun addAll(vararg items: T, inSection: Int) {
+        val asList = items.asList()
+        allDataCollection.addAll(asList, inSection)
+        super.addAll(asList.filterByOurFilter(), inSection)
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="remove">
     override fun remove(newItem: T, inSection: Int): Int? {
         allDataCollection.removeItem(newItem, inSection)
-        return super.remove(newItem, inSection)
+        var result: Int? = null
+        performActionIfIsValidFilter(newItem) {
+            result = super.remove(newItem, inSection)
+        }
+        return result
     }
 
     override fun removeAt(row: Int, inSection: Int) {
+        val item = getItem(row, inSection) ?: return
         allDataCollection.removeAt(row, inSection)
-        super.removeAt(row, inSection)
+        performActionIfIsValidFilter(item) {
+            super.removeAt(row, inSection)
+        }
+    }
+
+    override fun removeIn(range: IntRange, inSection: Int) {
+        if (filterValue != null) {
+            TODO("this is broken, should convert to list of items to remove the \"unfiltered\" once or more advancly," +
+                    " call removeAt for each (of the accepted) (in reverse order)")
+        }
+
+        allDataCollection.removeInRange(range, inSection)
+        super.removeIn(range, inSection)
+    }
+
+    override fun removeAll(items: List<T>, inSection: Int) {
+        allDataCollection.removeItems(items, inSection)
+        super.removeAll(items.filterByOurFilter(), inSection)
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="insert">
+    override fun insert(item: T, atRow: Int, inSection: Int) {
+        allDataCollection.insert(item, atRow, inSection)
+        performActionIfIsValidFilter(item) {
+            super.insert(item, atRow, inSection)
+        }
     }
 
     override fun insertAll(items: Collection<T>, startPosition: Int, inSection: Int) {
         allDataCollection.insertAll(items, startPosition, inSection)
-        super.insertAll(items, startPosition, inSection)
+        super.insertAll(items.filterByOurFilter(), startPosition, inSection)
     }
 
     override fun insertAll(vararg items: T, startPosition: Int, inSection: Int) {
         val asList = items.asList()
         allDataCollection.insertAll(asList, startPosition, inSection)
-        super.insertAll(asList, startPosition, inSection)
+        super.insertAll(asList.filterByOurFilter(), startPosition, inSection)
     }
+    //</editor-fold>
 
-    override fun addAll(vararg items: T, inSection: Int) {
-        val asList = items.asList()
-        allDataCollection.addAll(asList, inSection)
-        super.addAll(asList, inSection)
-    }
-
+    //<editor-fold desc="replace">
     override fun replace(newItem: T, position: Int, inSection: Int) {
         allDataCollection.replace(newItem, position, inSection)
-        super.replace(newItem, position, inSection)
+        performActionIfIsValidFilter(newItem) {
+            super.replace(newItem, position, inSection)
+        }
     }
+    //</editor-fold>
 
-    override fun removeIn(range: IntRange, inSection: Int) {
-        allDataCollection.removeInRange(range, inSection)
-        super.removeIn(range, inSection)
-    }
-
-
+    //<editor-fold desc="clear">
     override fun clear() {
         allDataCollection.clear()
         super.clear()
     }
+    //</editor-fold>
 
+    //<editor-fold desc="filter">
     private fun isAcceptedByFilter(newItem: T?, value: F?): Boolean {
         if (value == null || newItem == null) {
             return true
@@ -159,21 +200,18 @@ open class AbstractSearchableDataBindingRecyclerAdapter<
         filterActor.offer(newFilter)
     }
 
-    private suspend fun updateVisibly(data: List<TypeSection<T>>) {
-        launch(UI) {
-            super.setAllSections(data)
-        }.join()
-    }
 
     fun removeFilter() {
         filterValue = null
         filterActor.offer(null)
     }
 
+    fun getFilter(): F? = filterValue
+    //</editor-fold>
+
+    //<editor-fold desc="recycler view integration">
     override fun onAttachedToRecyclerView(recyclerView: RecyclerView?) {
-        filterActor.setup {
-            filterBySuspend(it)
-        }
+        filterActor.setup { filterBySuspend(it) }
         super.onAttachedToRecyclerView(recyclerView)
     }
 
@@ -181,17 +219,12 @@ open class AbstractSearchableDataBindingRecyclerAdapter<
         filterActor.clear()
         super.onDetachedFromRecyclerView(recyclerView)
     }
+    //</editor-fold>
 
-    fun getFilter(): F? = filterValue
-
-    override fun insert(item: T, atRow: Int, inSection: Int) {
-        allDataCollection.insert(item, atRow, inSection)
-        super.insert(item, atRow, inSection)
-    }
-
+    //<editor-fold desc="Section operations">
     override fun setSection(items: List<T>, inSection: Int) {
         allDataCollection.setSection(items, inSection)
-        super.setSection(items, inSection)
+        super.setSection(items.filterByOurFilter(), inSection)
     }
 
 
@@ -204,13 +237,23 @@ open class AbstractSearchableDataBindingRecyclerAdapter<
         allDataCollection.acceptSection(sectionIndex)
         super.showSection(sectionIndex)
     }
+    //</editor-fold>
 
-    override fun removeAll(items: List<T>, inSection: Int) {
-        allDataCollection.removeItems(items, inSection)
-        super.removeAll(items, inSection)
+    //<editor-fold desc="Helper functions">
+    private inline fun performActionIfIsValidFilter(newItem: T, crossinline action: FunctionUnit<T>) {
+        if (isAcceptedByFilter(newItem, filterValue)) {
+            action(newItem)
+        }
     }
+
+    private fun Collection<T>.filterByOurFilter(): List<T> =
+            filter { isAcceptedByFilter(it, filterValue) }
+    //</editor-fold>
 }
 
+/**
+ * Creates a single edtion of the conflator (so multiple calls to setup results in only 1 been made).
+ */
 private class ConflatedActorHelper<F> {
 
     private var eventActor: ActorJob<F?>? = null
@@ -223,6 +266,8 @@ private class ConflatedActorHelper<F> {
         if (eventActor != null) {
             return
         }
+
+
         eventActor = actor(CommonPool, capacity = Channel.CONFLATED) {
             channel.consumeEach(callback)
         }
@@ -231,7 +276,6 @@ private class ConflatedActorHelper<F> {
     fun clear() {
         eventActor = null
     }
-
 }
 
 

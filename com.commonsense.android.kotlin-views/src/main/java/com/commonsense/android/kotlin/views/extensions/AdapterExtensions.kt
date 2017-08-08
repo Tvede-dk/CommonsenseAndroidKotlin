02f46850ad7c441d46483157ddc98e4c1b133510
Kasper Tvede <kasper@webmasteren.eu>
2017-08-08 09:35:39 +0200

    minor helping extension, for when wanting to listen for when past first item in recyclerview.

--- com.commonsense.android.kotlin-views/src/main/java/com/commonsense/android/kotlin/views/extensions/AdapterExtensions.kt
+++ com.commonsense.android.kotlin-views/src/main/java/com/commonsense/android/kotlin/views/extensions/AdapterExtensions.kt
@@ -1,6 +1,7 @@
 package com.commonsense.android.kotlin.views.extensions
 
 import android.support.v7.widget.RecyclerView
+import com.commonsense.android.kotlin.base.FunctionUnit
 
 /**
  * Created by Kasper Tvede on 31-05-2017.
@@ -8,4 +9,20 @@ import android.support.v7.widget.RecyclerView
 fun RecyclerView.setup(adapter: RecyclerView.Adapter<*>, layoutManager: RecyclerView.LayoutManager) {
     this.adapter = adapter
     this.layoutManager = layoutManager
+}
+
+
+inline fun RecyclerView.addOnScrollWhenPastFirstItem(crossinline action: FunctionUnit<Boolean>): RecyclerView.OnScrollListener {
+    val listener = object : RecyclerView.OnScrollListener() {
+        override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+            if (recyclerView == null) {
+                return
+            }
+            val isFirstVisible = recyclerView.findViewHolderForLayoutPosition(0) != null
+            action(isFirstVisible)
+        }
+    }
+    addOnScrollListener(listener)
+    return listener
 }

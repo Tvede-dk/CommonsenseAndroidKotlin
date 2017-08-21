5bccd864e21aa6ee999cb0c796f65f1bfcb280d1
Kasper Tvede <kasper@webmasteren.eu>
2017-08-22 00:37:56 +0200

    minor things, still not done.

--- com.commonsense.android.kotlin-views/src/main/java/com/commonsense/android/kotlin/views/input/SingleSelectionViewHandler.kt
+++ com.commonsense.android.kotlin-views/src/main/java/com/commonsense/android/kotlin/views/input/SingleSelectionViewHandler.kt
@@ -5,10 +5,31 @@ package com.commonsense.android.kotlin.views.input
  */
 class SingleSelectionViewHandler<T> {
     // use toggleable view
-    var viewsToWorkOn = mutableListOf<ToggleableView<T>>()
+    private val viewsToWorkOn = mutableListOf<ToggleableView<T>>()
 
     private var lastSelected: ToggleableView<T>? = null
     private var selected: ToggleableView<T>? = null
 
+    fun addView(view: ToggleableView<T>) {
+        view.setOnSelectionChanged { onSelectionChanged(view, it) }
+    }
+
+    private var isAllowedToUnselectedAll: Boolean = false
+
+    private fun onSelectionChanged(view: ToggleableView<T>, it: Boolean) {
+        if (it) {
+            //we have changed selection, update old, and move "down" the chain.
+            selected?.isSelected = false
+            lastSelected = selected
+            selected = view
+        } else {
+            //by default it will be unselected, however if that is not allowed,
+            // then we are to correct that "mistake"
+            if (!isAllowedToUnselectedAll) {
+                view.isSelected = true
+            }
+        }
+
+    }
 
 }

911eb3b6761f7382e5badedda2f8e2019d9bd7ef
Kasper Tvede <kasper@webmasteren.eu>
2017-09-26 23:07:50 +0200

    added fromHtml extension on string. with newer api handling (> N )

---
+++ com.commonsense.android.kotlin-system/src/main/java/com/commonsense/android/kotlin/system/compat/SpannedCompat.kt
@@ -0,0 +1,20 @@
+package com.commonsense.android.kotlin.system.compat
+
+import android.annotation.SuppressLint
+import android.text.Html
+import android.text.Spanned
+import com.commonsense.android.kotlin.system.extensions.isApiOverOrEqualTo
+
+
+/**
+ * Created by kasper on 26/09/2017.
+ */
+@SuppressLint("NewApi")
+@SuppressWarnings("deprecation")
+fun String.fromHtml(): Spanned {
+    (return if (isApiOverOrEqualTo(24)) {
+        Html.fromHtml(this, Html.FROM_HTML_MODE_LEGACY)
+    } else {
+        Html.fromHtml(this)
+    })
+}

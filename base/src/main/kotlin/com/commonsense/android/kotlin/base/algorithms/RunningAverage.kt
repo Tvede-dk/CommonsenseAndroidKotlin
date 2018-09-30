052cc8242612b78f861b000a600a672bb55e29bf
kasper Tvede <kasper@webmasteren.eu>
2018-09-30 14:13:11 +0200

    more work on running average .

--- base/src/main/kotlin/com/commonsense/android/kotlin/base/algorithms/RunningAverage.kt
+++ base/src/main/kotlin/com/commonsense/android/kotlin/base/algorithms/RunningAverage.kt
@@ -36,18 +36,28 @@ abstract class RunningAverageAbstract<T : Number> {
     }
 }
 
+
+/**
+ *
+ */
 open class RunningAverageInt : RunningAverageAbstract<Int>() {
     override fun addValues(first: Int, second: Int): Int = first + second
     override val zero: Int
         get() = 0
 }
 
+/**
+ *
+ */
 open class RunningAverageDouble : RunningAverageAbstract<Double>() {
     override fun addValues(first: Double, second: Double): Double = first + second
     override val zero: Double
         get() = 0.0
 }
 
+/**
+ *
+ */
 open class RunningAverageFloat : RunningAverageAbstract<Float>() {
     override fun addValues(first: Float, second: Float): Float = first + second
     override val zero: Float
@@ -55,6 +65,92 @@ open class RunningAverageFloat : RunningAverageAbstract<Float>() {
 }
 
 
+abstract class RunningAverageCappedAbstract<T : Number>(
+        private val cappedNumberOfValues: Int) {
+
+
+    abstract fun clearValues()
+
+    abstract fun setValue(item: T, index: Int)
+
+    abstract fun takeValues(toTakeCount: Int): Iterable<T>
+
+
+    /**
+     * How many values we have set of the available elements in the array
+     */
+    private var valuesSet = 0
+    /**
+     * Since the array acts as a ring buffer.
+     */
+    private var currentIndex = 0
+
+
+    fun addValue(newValue: T) {
+        valuesSet = minOf(valuesSet + 1, cappedNumberOfValues)
+        setValue(newValue, currentIndex)
+        currentIndex = (currentIndex + 1).rem(cappedNumberOfValues)
+    }
+
+    val average: Double
+        get() = takeValues(valuesSet).sumByDouble(Number::toDouble) / valuesSet
+
+    fun reset() {
+        valuesSet = 0
+        currentIndex = 0
+        clearValues()
+    }
+
+}
+
+
+open class RunningAverageFloatCapped2(cappedValuesToAverage: Int) : RunningAverageCappedAbstract<Float>(cappedValuesToAverage) {
+
+    private val values = FloatArray(cappedValuesToAverage)
+
+    override fun takeValues(toTakeCount: Int) = values.take(toTakeCount)
+
+    override fun setValue(item: Float, index: Int) {
+        values[index] = item
+    }
+
+    override fun clearValues() = values.fill(0f)
+}
+
+open class RunningAverageIntCapped(cappedValuesToAverage: Int) : RunningAverageCappedAbstract<Int>(cappedValuesToAverage) {
+
+    private val values = IntArray(cappedValuesToAverage)
+
+    override fun clearValues() = values.fill(0)
+
+    override fun setValue(item: Int, index: Int) {
+        values[index] = item
+    }
+
+    override fun takeValues(toTakeCount: Int): Iterable<Int> = values.take(toTakeCount)
+}
+
+open class RunningAverageDoubleCapped(cappedValuesToAverage: Int) : RunningAverageCappedAbstract<Double>(cappedValuesToAverage) {
+
+    private val values = DoubleArray(cappedValuesToAverage)
+
+    override fun clearValues() = values.fill(0.0)
+
+
+    override fun setValue(item: Double, index: Int) {
+        values[index] = item
+    }
+
+
+    override fun takeValues(toTakeCount: Int): Iterable<Double> =
+            values.take(toTakeCount)
+
+}
+
+
+/**
+ *
+ */
 class RunningAverageFloatCapped(
         private val cappedValuesToAverage: Int) {
 
